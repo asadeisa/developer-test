4fd64757a1ee2f23cce581717a6e85f798656632
asad eisa <asadeisa232@gmail.com>
2024-08-14 18:45:42 +0300

    setting up

--- app.vue
+++ app.vue
@@ -1,6 +1,32 @@
+<script setup>
+
+useHead({
+  title: 'test app',
+  meta: [
+    { name: 'description', content: 'My amazing site.' }
+  ],
+
+  htmlAttrs: {
+    lang: `en`,
+    // class: 'dark1'
+  },
+
+  link: [
+    {
+      rel: 'stylesheet',
+      href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css',
+      integrity: 'sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC',
+      crossorigin: 'anonymous'
+    }
+
+  ],
+})
+
+</script>
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <NuxtLayout>
+      <NuxtPage></NuxtPage>
+    </NuxtLayout>
   </div>
 </template>
---
+++ layouts/default.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    this is layout
+    <slot></slot>
+  </div>
+</template>
